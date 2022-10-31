ad1e7f868148a78e09b01f3b56005ce2ba5d7f0e
ZalwayJH <joelhaughton94@gmail.com>
2022-10-31 14:06:56 +0000

    added info from feeback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
-import { Routes } from "react-router-dom";
 import React from "react";
 import Header from "./Components/Header";
-import Nav from "./Components/Nav";
 import ArticleList from "./Components/ArticleList";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Nav />
       <ArticleList />
     </div>
   );
