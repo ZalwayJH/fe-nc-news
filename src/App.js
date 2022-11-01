2d1510e291a85d53291a4ba88d03bf36d6f60c0e
ZalwayJH <joelhaughton94@gmail.com>
2022-11-01 16:03:30 +0000

    seperated the fetch topics function to the Topics component from the App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 import Header from "./Components/Header";
 import ArticleList from "./Components/ArticleList";
 import Topics from "./Components/Topics";
@@ -7,25 +7,11 @@ import Topics from "./Components/Topics";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [selectTopics, setTopics] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(`https://project-northcoders-nc-news.herokuapp.com/api/topics`)
-      .then((res) => res.json())
-      .then((data) => {
-        const { topics } = data;
-
-        setTopics(topics);
-        setLoading(false);
-      });
-  }, [isLoading]);
-
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Topics selectTopics={selectTopics} />
+        <Topics />
         <Routes>
           <Route path="/" element={<ArticleList />} />
           <Route path="/:topic" element={<ArticleList />}></Route>
--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -1,7 +1,21 @@
 import { Link } from "react-router-dom";
-import React from "react";
+import { React, useState, useEffect } from "react";
 
-const Topics = ({ selectTopics }) => {
+const Topics = () => {
+  const [selectTopics, setTopics] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://project-northcoders-nc-news.herokuapp.com/api/topics`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { topics } = data;
+
+        setTopics(topics);
+        setLoading(false);
+      });
+  }, [isLoading]);
+
   const allTopics = selectTopics.map((item) => {
     return item.slug;
   });
